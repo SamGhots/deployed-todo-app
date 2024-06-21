bd153b6de8ed1062079bc921de137e5e2f90c883
todsapon meepon <111333915+SamGhots@users.noreply.github.com>
2024-06-21 15:09:41 +0700

    Update db.js
    rename database

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ const pool= new Pool({
     password:process.env.PASSWORD,
     host:process.env.HOST,
     port:process.env.DBPORT,
-    database:'todolist'
+    database:'todoapp'
 })
 
 module.exports = pool
